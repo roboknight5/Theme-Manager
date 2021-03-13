092bb2a4e8181680e56fa31d876f793c9c21c549
dev_Mohamed_Gougou <roboknight5@yahoo.com>
2021-03-13 20:01:23 +0200

    adding stuff to layoutUI

--- Layout/AddLayoutUI.cs
+++ Layout/AddLayoutUI.cs
@@ -4,6 +4,9 @@ namespace Gtk_Theme_Manager
 {
     public class AddLayoutUI :VBox
     {
-        
+        public AddLayoutUI()
+        {
+            Add(new Label("Add Layout"));
+        }
     }
 }
--- Layout/ViewLayoutUI.cs
+++ Layout/ViewLayoutUI.cs
@@ -4,6 +4,9 @@ namespace Gtk_Theme_Manager
 {
     public class ViewLayoutUI :VBox
     {
-        
+        public ViewLayoutUI()
+        {
+            Add(new Label("View Layout"));
+        }
     }
 }
